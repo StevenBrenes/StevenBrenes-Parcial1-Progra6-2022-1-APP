0bfa3e38bf5eb32f3fd89640c7ae2f293125b17c
Steven Brenes <sbgdp@outlook.com>
2022-03-16 20:07:43 -0600

    Agregar archivos de proyecto.

---
+++ StevenBrenes-Parcial1-Progra6-2022-1-APP/StevenBrenes-Parcial1-Progra6-2022-1-APP/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StevenBrenes_Parcial1_Progra6_2022_1_APP.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
